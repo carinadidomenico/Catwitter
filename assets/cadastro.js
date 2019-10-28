8ec965f0e045797c44c15fa73bf9411d9b6c37c2
carinadidomenico <carinadidomenico@gmail.com>
2019-10-28 18:56:55 -0200

    Deletando script extra

--- assets/cadastro.js
+++
@@ -1,9 +0,0 @@
-const nomeCadastro = document.querySelector ("#cadastro__nome");
-const senhaCadastro = document.querySelector ("#cadastro__nome");
-const formCadastro = document.querySelector ("#cadastro__form");
-
-// CADASTRO
-
-formCadastro.addEventListener ('submit', function (){
-    
-}); 
