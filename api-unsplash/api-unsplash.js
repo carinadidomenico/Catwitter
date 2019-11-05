a2b3a4e99fa9329905687e5fc1aead1d22f4e017
carinadidomenico <carinadidomenico@gmail.com>
2019-11-05 00:56:25 -0200

    Adicionando mais fotos

--- api-unsplash/api-unsplash.js
+++ api-unsplash/api-unsplash.js
@@ -1,7 +1,7 @@
 const catImages = document.querySelector(".cat__images");
 
 async function getImages(){
-    let response = await fetch('https://api.unsplash.com/search/photos?query=cats&client_id=676f87eea05b764fd60059722d018ed889ef90819dfc1b22e9f6e562e2868aed&orientation=landscape&per_page=20');
+    let response = await fetch('https://api.unsplash.com/search/photos?query=cats&client_id=676f87eea05b764fd60059722d018ed889ef90819dfc1b22e9f6e562e2868aed&orientation=landscape&per_page=25');
     let jsonResponse = await response.json();
     let imagesList = await jsonResponse.results;
     createImages(imagesList);
